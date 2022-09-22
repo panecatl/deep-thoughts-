81bca45721097d2732645ac91ac694304b4ce1b8
panecatl <ypanecatl24@gmail.com>
2022-09-21 19:48:47 -0500

    finished login feature

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -13,11 +19,21 @@ const Signup = () => {
     });
   };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-  };
+ // submit form (notice the async!)
+const handleFormSubmit = async (event) => {
+  event.preventDefault();
 
+  // use try/catch instead of promises to handle errors
+  try {
+    // execute addUser mutation and pass in variable data from form
+    const { data } = await addUser({
+      variables: { ...formState }
+    });
+    Auth.login(data.addUser.token);
+  } catch (e) {
+    console.error(e);
+  }
+};
   return (
     <main className='flex-row justify-center mb-4'>
       <div className='col-12 col-md-6'>
@@ -56,6 +72,8 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+
+            {error && <div>Signup failed</div>}
           </div>
         </div>
       </div>
